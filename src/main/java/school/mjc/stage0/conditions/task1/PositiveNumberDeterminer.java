bc4c3d2ef40fd6c9cfc08aa0e7fe9bdde58a4a5a
MKN2003 <kazimovmiraziz197@gmail.com>
2023-05-11 22:15:03 +0500

    Changes have been made.

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
-        numberToBeDetermined = -1;
-        boolean res1 = true; boolean res2 = false;
+        boolean res1 = true;
+        boolean res2 = false;
 
         if(numberToBeDetermined > 0) {
             System.out.println(res1);
